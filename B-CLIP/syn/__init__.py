4ce904b64558bb3af98d67e772e21431e8cd431f
Zifeng Lian <71648960+LianZifeng@users.noreply.github.com>
2025-12-09 16:26:26 +0800

    Delete B-CLIP directory

--- B-CLIP/syn/__init__.py
+++
@@ -1,4 +0,0 @@
-from .PET_syn import GMM_pet
-from .MRI_Syn import GMM_mri_t1,GMM_mri_t2, GMM_DWI_flair
-from .random_syn import GMM_random
-from .lesion_Syn import GMM_ct_lesion, GMM_DWI_flair_lesion, real_ct_lesion, real_DWI_flair_lesion
